f86ed11b7c1ecc166ae39424ee410e05ff0bf561
Barış Saylık <154532832+gfbaris@users.noreply.github.com>
2024-08-28 11:31:38 +0300

    SaglikGideri.java

---
+++ SaglikGideri.java
@@ -0,0 +1,13 @@
+public class SaglikGideri {
+    private String isim;
+    private double ucret;
+
+    public SaglikGideri(String isim, double ucret) {
+        this.isim = isim;
+        this.ucret = ucret;
+    }
+
+    public double getUcret() {
+        return ucret;
+    }
+}
