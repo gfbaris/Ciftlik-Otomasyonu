950cbab1ce482f22b985f6b8445569b512554011
Barış Saylık <154532832+gfbaris@users.noreply.github.com>
2024-08-28 11:32:01 +0300

    Yem.java

---
+++ Yem.java
@@ -0,0 +1,13 @@
+public class Yem {
+    private String yemAdi;
+    private double yemFiyati;
+
+    public Yem(String yemAdi, double yemFiyati) {
+        this.yemAdi = yemAdi;
+        this.yemFiyati = yemFiyati;
+    }
+
+    public double getYemFiyati() {
+        return yemFiyati;
+    }
+}
